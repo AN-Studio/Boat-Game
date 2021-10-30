4ea987cc3677c06f6a51514748cdcba8ad4c944f
Jay Kozatt <jaykozatt@gmail.com>
2021-10-29 21:59:29 -0400

    Added parallax effect

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public Transform cam;
+    public float parallaxFactor;
+    
+    protected float length; 
+    protected float scaledLength;
+    protected Vector2 origin;
+
+    void Awake() 
+    {
+        cam = Camera.main.transform;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        scaledLength = length * transform.localScale.x;
+        origin = transform.position;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = cam.position.x * (1 - parallaxFactor);
+        float distance = cam.position.x * parallaxFactor;
+
+        transform.position = new Vector3(origin.x + distance, origin.y, transform.position.z);
+
+        if (temp > origin.x + scaledLength) origin.x += length;
+        else if (temp < origin.x - scaledLength) origin.x -= length; 
+    }
+}
