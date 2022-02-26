4d7e5090613fb6923a28a2636799737762fe90be
Jay Kozatt <jaykozatt@gmail.com>
2022-02-26 01:37:32 -0400

    Reorganized sorting layers

--- Assets/Scripts/Utilities/WaveMotion.cs
+++ Assets/Scripts/Utilities/WaveMotion.cs
@@ -7,25 +7,35 @@ public class WaveMotion : MonoBehaviour
     Transform cam;
     Vector3 origin;
     float length;
+    [Range(0,1)] public float parallaxFactor;
     public Vector2 ellipse;
     Vector2 motion;
 
+    float height;
+    float width;
     void Awake() {
         cam = Camera.main.transform;
         motion = Vector2.zero;
         length = GetComponentInChildren<SpriteRenderer>().bounds.size.x;
         origin = transform.position;
+
+        
+        height = 2 * Mathf.Tan(.5f * Camera.main.fieldOfView) * Mathf.Abs(transform.position.z - cam.position.z);
+        width = height * (Screen.width / Screen.height);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        float temp = cam.position.x;
+        // float distance = cam.position.x * parallaxFactor;
+
         motion.x = ellipse.x * Mathf.Cos(Time.time);
         motion.y = ellipse.y * Mathf.Sin(Time.time);
 
-        transform.position = new Vector3(origin.x + cam.position.x + motion.x, origin.y + motion.y, transform.position.z);
+        transform.position = new Vector3(origin.x + motion.x, origin.y + motion.y, transform.position.z);
 
-        if (cam.position.x > origin.x + length) origin.x += length;
-        else if (cam.position.x < origin.x - length) origin.x -= length; 
+        if (temp > origin.x + length/width) origin.x += length;
+        else if (temp < origin.x - length/width) origin.x -= length; 
     }
 }
