1cf234d49b077bb9cdceda2eef1df09065d7e33a
Jay Kozatt <jaykozatt@gmail.com>
2022-06-01 18:07:44 -0400

    Added ocean floor & background fog

--- Assets/_Scripts/Utilities/Parallax.cs
+++ Assets/_Scripts/Utilities/Parallax.cs
@@ -4,37 +4,33 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    public Transform cam;
-    public float parallaxFactor;
-    
-    protected float length; 
-    // protected float scaledLength;
-    protected Vector2 origin;
+    protected Transform cam;
+    protected Vector3 origin;
+    protected float length;
+    [Range(0,1)] public float parallaxFactor;
+    protected float height;
+    protected float width;
 
-    void Awake() 
+    protected virtual void Awake() 
     {
         cam = Camera.main.transform;
         length = GetComponentInChildren<SpriteRenderer>().bounds.size.x;
-        // scaledLength = length * transform.lossyScale.x;
         origin = transform.position;
 
-    }
+        height = 2 * Mathf.Tan(.5f * Camera.main.fieldOfView) * Mathf.Abs(transform.position.z - cam.position.z);
+        width = height * (Screen.width / Screen.height);
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    protected virtual void FixedUpdate()
     {
         float temp = cam.position.x * (1 - parallaxFactor);
         float distance = cam.position.x * parallaxFactor;
 
         transform.position = new Vector3(origin.x + distance, origin.y, transform.position.z);
 
-        if (temp > origin.x + length) origin.x += length;
-        else if (temp < origin.x - length) origin.x -= length; 
+        if (temp > origin.x + length/width) origin.x += length;
+        else if (temp < origin.x - length/width) origin.x -= length;
     }
 }
--- Assets/_Scripts/Utilities/WaveMotion.cs
+++ Assets/_Scripts/Utilities/WaveMotion.cs
@@ -2,30 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveMotion : MonoBehaviour
+public class WaveMotion : Parallax
 {
-    Transform cam;
-    Vector3 origin;
-    float length;
-    [Range(0,1)] public float parallaxFactor;
     public Vector2 ellipse;
     Vector2 motion;
 
-    float height;
-    float width;
-    void Awake() {
-        cam = Camera.main.transform;
-        motion = Vector2.zero;
-        length = GetComponentInChildren<SpriteRenderer>().bounds.size.x;
-        origin = transform.position;
-
-        
-        height = 2 * Mathf.Tan(.5f * Camera.main.fieldOfView) * Mathf.Abs(transform.position.z - cam.position.z);
-        width = height * (Screen.width / Screen.height);
-    }
-
     // Update is called once per frame
-    void FixedUpdate()
+    protected override void FixedUpdate()
     {
         float temp = cam.position.x;
         // float distance = cam.position.x * parallaxFactor;
